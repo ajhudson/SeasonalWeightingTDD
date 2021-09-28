4a3f280caa6b8dd1d3c770f91c10cd81a33b0906
Andrew Hudson <andrew.hudson@esgglobal.com>
2021-09-28 15:47:48 +0100

    Add an interface to represent access to a database

---
+++ SeasonalWeighting.Lib/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SeasonalWeighting.Lib
+{
+    public interface IRepository
+    {
+        Task<int> GetSeasonalWeightingForMonthAsync(int monthNumber);
+    }
+}

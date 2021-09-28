64065de7915bbcba988c0af1b692624ae97b025d
Andrew Hudson <andrew.hudson@esgglobal.com>
2021-09-28 11:49:10 +0100

    Added unit test for scenario 2

--- SeasonalWeighting.Tests/SeasonalWeightingCalculatorTests.cs
+++ SeasonalWeighting.Tests/SeasonalWeightingCalculatorTests.cs
@@ -16,7 +16,7 @@ namespace SeasonalWeighting.Tests
         }
 
         [Test]
-        public void ShouldCalcWhenSeasonalWeightingIsPercentageIncreaseOfAnnualQuantity()
+        public void ShouldReturnCorrectResultForScenario1()
         {
             // Arrange
             int annualQty = 36500;
@@ -27,6 +27,20 @@ namespace SeasonalWeighting.Tests
 
             // Assert
             result.ShouldBe(3720.0m);
+        }
+
+        [Test]
+        public void ShouldReturnCorrectResultForScenario2()
+        {
+            // Arrange
+            int annualQty = 36500;
+            int seasonalWeighting = -20;
+
+            // Act
+            decimal result = this._seasonalWeightingCalculator.Estimate(annualQty, seasonalWeighting);
+
+            // Assert
+            result.ShouldBe(2480m);
         }
     }
 }
